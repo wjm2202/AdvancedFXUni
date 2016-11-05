b06e674485aa8eac9406e4a6627623be12622995
wjm2202 <wjm2202@aut.ac.nz>
2016-11-05 19:34:24 +1300

    start on javaFX game using obj models

---
+++ src/nz/ac/aut/wjm2202/three/GameThree.java
@@ -0,0 +1,25 @@
+package nz.ac.aut.wjm2202.three;
+/**
+ * Learning material from Pro JavaFX
+ * code used for educational purposes
+ */
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.PixelWriter;
+import javafx.scene.image.WritableImage;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+
+public class GameThree extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/nz/ac/aut/wjm2202/three/GameThreeController.java
@@ -0,0 +1,18 @@
+package nz.ac.aut.wjm2202.three;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+ * Created by Liandri on 5/11/2016.
+ */
+public class GameThreeController {
+
+
+}
